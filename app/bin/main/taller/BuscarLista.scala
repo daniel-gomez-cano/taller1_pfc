a13b1fe9f2ea19c8daed1cf603598c2fd1e2e244
F4RGAN2007 <avalo.santiago@correounivalle.edu.co>
2024-09-12 20:32:25 -0500

    metodo max recurcion y clase TorresHanoi

--- app/bin/main/taller/BuscarLista.scala
+++ app/bin/main/taller/BuscarLista.scala
@@ -7,4 +7,26 @@ class BuscarLista() {
     case x :: xs => if (x == elemento) true else buscarElemento(xs, elemento)
    }
   }
+
+  //metodo para hallar el mayor valor en una lista no vacia de enteros positivos
+  //Recurcion de cola
+  def maxLin(l: List[Int]): Int = {
+    require(!l.isEmpty, "La lista no puede estar vacia")
+    require(l.forall(_ >= 0), "La lista no puede contener números negativos")
+    @tailrec
+    def maxLi_nAux ( lista: List[Int] , max: Int ): Int = {
+
+      if(lista.isEmpty) max
+      else if (lista.head > max) maxLi_nAux(lista.tail, lista.head)
+      else maxLi_nAux(lista.tail, max)
+      
+    }
+
+    maxLi_nAux(l.tail, l.head)
+
+  }
+
+  def minLin(l: List[Int]): Int = {
+    0
+  }
 }
